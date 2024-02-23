70956c124174113416c1e973c66568f698783ae4
Petar Popovic <petarpnp@gmail.com>
2024-02-23 11:51:32 +0100

    Add boilerplate

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,137 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ["latin"] });
+const TokenSelector = () => {
+  const options = {
+    eth: "ETH",
+    usdc: "USDC",
+  }
+
+  return (
+    <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Select token" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value={options.eth}>
+          <div className="flex gap-[7px] items-center">
+            <img className="w-5" src="/eth-logo.svg" alt="ETH logo" />
+            {options.eth}
+          </div>
+        </SelectItem>
+        <SelectItem value={options.usdc}>
+          <div className="flex gap-[7px] items-center">
+            <img className="w-5" src="/usdc-logo.svg" alt="USDC logo" />
+            {options.usdc}
+          </div>
+        </SelectItem>
+      </SelectContent>
+    </Select>
+  )
+}
+
+const SwapForm = () => (
+  <div className="mt-5">
+    <Label htmlFor="youPay">You pay</Label>
+    <div className="flex items-center gap-[20px] mb-5">
+      <Input id="youPay" placeholder="0" />
+      <TokenSelector />
+    </div>
+    <Label htmlFor="youReceive">You receive</Label>
+    <div className="flex items-center gap-[20px]">
+      <Input id="youReceive" placeholder="0" />
+      <TokenSelector />
+    </div>
+    <Button size="lg" className="w-full mt-5">Swap</Button>
+  </div>
+);
+
+const LendForm = () => (
+  <div className="mt-5">
+    <Label htmlFor="amount">Amount to supply</Label>
+    <div className="flex items-center gap-[20px] mb-5">
+      <Input id="amount" placeholder="0" />
+      <TokenSelector />
+    </div>
+    <Button size="lg" className="w-full mt-5">Lend</Button>
+  </div>
+);
+
+const SwapTab = () => (
+  <Card className="mt-10">
+    <CardHeader>
+      <CardTitle>Choose which protocol you want to use to swap tokens</CardTitle>
+    </CardHeader>
+    <CardContent>
+      <Tabs defaultValue="uniswap">
+        <TabsList >
+          <TabsTrigger className="flex gap-[7px] items-center" value="uniswap">
+            <img className="w-5" src="/uniswap-uni-logo.png" alt="Uniswap logo" />
+            Uniswap
+          </TabsTrigger>
+          <TabsTrigger className="flex gap-[7px] items-center" value="curve">
+            <img className="w-5" src="/curve-logo.png" alt="Curve logo" />
+            Curve
+          </TabsTrigger>
+          <TabsTrigger className="flex gap-[7px] items-center" value="balancer">
+            <img className="w-5" src="/balancer-logo.png" alt="Balancer logo" />
+            Balancer
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="uniswap">
+          <SwapForm />
+        </TabsContent>
+        <TabsContent value="curve">
+          <SwapForm />
+        </TabsContent>
+        <TabsContent value="balancer">
+          <SwapForm />
+        </TabsContent>
+      </Tabs>
+    </CardContent>
+  </Card>
+);
+
+const LendTab = () => (
+  <Card className="mt-10">
+    <CardHeader>
+      <CardTitle>Lend your assets on Aave</CardTitle>
+    </CardHeader>
+    <CardContent>
+      <LendForm />
+    </CardContent>
+  </Card>
+);
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main>
+      <Head>
+        <title>Shell Integrateooor</title>
+      </Head>
+      <p className="font-bold text-2xl text-center py-10 text-gray-600">Shell Integrateooor</p>
+      <div className="max-w-2xl mx-auto">
+        <Tabs defaultValue="swap">
+          <div className="flex justify-center">
+            <TabsList >
+              <TabsTrigger value="swap">Swap</TabsTrigger>
+              <TabsTrigger value="lend">Lend</TabsTrigger>
+            </TabsList>
+          </div>
+          <TabsContent value="swap">
+            <SwapTab />
+          </TabsContent>
+          <TabsContent value="lend">
+            <LendTab />
+          </TabsContent>
+        </Tabs>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
