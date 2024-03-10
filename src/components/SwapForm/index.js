2a65043fe3346e889cbb95cb6f35cc490fb02926
Petar Popovic <petarpnp@gmail.com>
2024-03-10 13:58:09 +0100

    Update SwapForm and TokenSelector components

--- src/components/SwapForm/index.js
+++ src/components/SwapForm/index.js
@@ -1,22 +1,37 @@
+import { useState } from "react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import TokenSelector from "@/components/TokenSelector";
+import { DAI_TOKEN_DATA, USDC_TOKEN_DATA } from "@/lib/constants";
 
-const SwapForm = () => (
-  <div className="mt-5">
-    <Label htmlFor="youPay">You pay</Label>
-    <div className="flex items-center gap-[20px] mb-5">
-      <Input id="youPay" placeholder="0" />
-      <TokenSelector />
+const SwapForm = () => {
+  const [selectedToken, setSelectedToken] = useState(DAI_TOKEN_DATA);
+
+  const tokenOptions = {
+    dai: DAI_TOKEN_DATA,
+    usdc: USDC_TOKEN_DATA,
+  };
+
+  return (
+    <div className="mt-5">
+      <Label htmlFor="youPay">You pay</Label>
+      <div className="flex items-center gap-[20px] mb-5">
+        <Input id="youPay" placeholder="0" />
+        <TokenSelector
+          options={tokenOptions}
+          selectedToken={selectedToken}
+          setSelectedToken={setSelectedToken}
+        />
+      </div>
+      <Label htmlFor="youReceive">You receive</Label>
+      <div className="flex items-center gap-[10px] mt-1">
+        <p>{0}</p>
+        <p>{ selectedToken.name === tokenOptions.dai.name ? tokenOptions.usdc.name : tokenOptions.dai.name }</p>
+      </div>
+      <Button size="lg" className="w-full mt-5">Swap</Button>
     </div>
-    <Label htmlFor="youReceive">You receive</Label>
-    <div className="flex items-center gap-[20px]">
-      <Input id="youReceive" placeholder="0" />
-      <TokenSelector />
-    </div>
-    <Button size="lg" className="w-full mt-5">Swap</Button>
-  </div>
-);
+  );
+}
 
 export default SwapForm;
--- src/components/TokenSelector/index.js
+++ src/components/TokenSelector/index.js
@@ -1,27 +1,25 @@
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { DAI_TOKEN_DATA, USDC_TOKEN_DATA } from "@/lib/constants";
 
-const TokenSelector = () => {
-  const options = {
-    eth: "ETH",
-    usdc: "USDC",
-  }
+const TokenSelector = ({options, selectedToken, setSelectedToken}) => {
+
 
   return (
-    <Select>
+    <Select value={selectedToken} onValueChange={setSelectedToken}>
       <SelectTrigger className="w-[180px]">
         <SelectValue placeholder="Select token" />
       </SelectTrigger>
       <SelectContent>
-        <SelectItem value={options.eth}>
+        <SelectItem value={options.dai}>
           <div className="flex gap-[7px] items-center">
-            <img className="w-5" src="/eth-logo.svg" alt="ETH logo" />
-            {options.eth}
+            <img className="w-5" src="/eth-logo.svg" alt="DAI logo" />
+            {options.dai.name}
           </div>
         </SelectItem>
         <SelectItem value={options.usdc}>
           <div className="flex gap-[7px] items-center">
             <img className="w-5" src="/usdc-logo.svg" alt="USDC logo" />
-            {options.usdc}
+            {options.usdc.name}
           </div>
         </SelectItem>
       </SelectContent>
